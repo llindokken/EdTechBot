4a56eb76bf9959120c42e4892f0b3639964d753e
llindokken <llindokken@gmail.com>
2021-02-05 16:31:07 -0600

    Added cogs directory. Renamed main.py to bot.py. Created cogs/classroom.py file.

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,17 @@
+# bot.py
+import dotenv
+import os
+import discord
+from discord.ext import commands
 
+
+
+dotenv.load('../.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Bot Command
+bot = commands.Bot(command_prefix='!')
+
+bot.load_extension("cogs.classroom")
+    
+bot.run(TOKEN)
